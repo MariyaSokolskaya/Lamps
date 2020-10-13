67db823e9bfd8b213dc3b521611fafd9986c0789
MariyaSokolskaya <faenna@inbox.ru>
2020-10-13 18:52:45 +0800

    TouchEvent

--- app/src/main/java/com/example/lamps/GameField.java
+++ app/src/main/java/com/example/lamps/GameField.java
@@ -3,7 +3,12 @@ package com.example.lamps;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.Random;
 
 public class GameField extends View {
 
@@ -11,15 +16,65 @@ public class GameField extends View {
     int fieldSizeN = 4;
     int fieldSizeM = 4;
     int r, ro; //радиус и расстояние между окружностями
+    float x, y;
+    float touchX, touchY;
     public GameField(Context context) {
         super(context);
-        //TODO сформировать массив случайно
-        //TODO рассчитать радиус и расстояние между окружностями
+        //сформировать массив случайно
+        Random random = new Random();
+        lampsState = new boolean[fieldSizeN][fieldSizeM];
+        for (int i = 0; i < lampsState.length; i++) {
+            for (int j = 0; j < lampsState[i].length; j++) {
+                lampsState[i][j] = random.nextBoolean();
+            }
+        }
+        //рассчитать радиус и расстояние между окружностями
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        //TODO отрисовать окружности согласно массиву состояний
+        r = canvas.getWidth()/(fieldSizeM * 4);
+        ro = r;
+        //отрисовать окружности согласно массиву состояний
+        Paint paint = new Paint();
+        paint.setColor(Color.BLUE);
+        paint.setStrokeWidth(4);
+        for (int i = 0; i < lampsState.length; i++) {
+            for (int j = 0; j < lampsState[i].length; j++) {
+                if (lampsState[i][j] == true) {
+                    paint.setStyle(Paint.Style.FILL_AND_STROKE);
+                }
+                else {
+                    paint.setStyle(Paint.Style.STROKE);
+                }
+                x = (j + 1) * (2 * r + ro);
+                y = (i + 1) * (2 * r + ro);
+                canvas.drawCircle(x, y, r, paint);
+            }
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN){
+            touchX = event.getX();
+            touchY = event.getY();
+            calculate();
+        }
+        return super.onTouchEvent(event);
+    }
+
+    void calculate(){
+        int i = Math.round(touchY / (2 * r + ro));
+        int j = Math.round(touchX / (2 * r + ro));
+        if (i<=4 && j<=4) {
+            x = j * (2 * r + ro);
+            y = i * (2 * r + ro);
+            if (Math.pow(touchX - x, 2) + Math.pow(touchY - y, 2) <= r * r) {
+                lampsState[i - 1][j - 1] = !lampsState[i - 1][j - 1];
+            }
+            invalidate();
+        }
     }
 }
